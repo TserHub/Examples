80a3642c170a46572aa805cf2612d6bbcfa9659f
qinWenMeng <qwm668@gmail.com>
2019-07-03 14:21:57 +0800

    --proxy

--- ES6/Proxy/Proxy.js
+++ ES6/Proxy/Proxy.js
@@ -2,33 +2,90 @@
  * ES6 原生提供 Proxy 构造函数，用来生成 Proxy 实例。
  * target参数表示所要拦截的目标对象
  * handler参数也是一个对象，用来定制拦截行为。
+ * get(target, propKey, receiver)：拦截对象属性的读取
+ * set(target, propKey, value, receiver)：拦截对象属性的设置
  */
-// var proxy = new Proxy(target, handler);
 
-let proxy = new Proxy({ test: 'hhh' }, {
-  get: function (target, key, receiver) {
-    console.log('%c-------get--------', 'color: red;');
-    console.log('target:', target);
-    console.log('key:', key);
-    console.log('receiver:', receiver);
-    return Reflect.get(target, key, receiver);
-  },
-  set: function (target, key, value, receiver) {
-    console.log('%c-------set--------', 'color: red;');
-    console.log('target:', target);
-    console.log('key:', key);
-    console.log('value:', value);
-    console.log('receiver:', receiver);
-    return Reflect.set(target, key, value, receiver);
-  }
-});
+/*
+let target = {};
+let handler = {
+  // target: 目标对象, property: 所要访问的属性
+  get: (target, property, receiver) => { },
+  // value: 设置的属性值
+  set: (target, property, value, receiver) => { },
+};
+let proxy = new Proxy(target, handler);
+*/
 
-console.log('%cproxy:', 'color:blue;', proxy);
+const divider = () => {
+  console.log('------------///------------');
+};
 
-proxy.count = 1;
+{
+  let target = { whoami: 'target' };
+  let proxy = new Proxy(target, {
 
-console.log('%cproxy:', 'color:blue;', proxy);
+    get: function (target, key, receiver) {
+      console.log('%c-------get--------', 'color: red;');
+      console.log('target:', target);
+      console.log('key:', key);
+      console.log('receiver:', receiver);
+      return Reflect.get(target, key, receiver);
+    },
+    set: function (target, key, value, receiver) {
+      console.log('%c-------set--------', 'color: red;');
+      console.log('target:', target);
+      console.log('key:', key);
+      console.log('value:', value);
+      console.log('receiver:', receiver);
+      return Reflect.set(target, key, value, receiver);
+    }
+  });
 
-++proxy.count;
+  console.log('%cproxy:', 'color:blue;', proxy);
 
-console.log('%cproxy:', 'color:blue;', proxy);
+  proxy.count = 1;
+
+  console.log('%cproxy:', 'color:blue;', proxy);
+  console.log('%ctarget:', 'color: blue;', target);
+
+  ++proxy.count;
+
+  console.log('%cproxy:', 'color:blue;', proxy);
+  console.log('%ctarget:', 'color: blue;', target);
+
+  divider();
+}
+
+/**
+ * 注意，要使得Proxy起作用，必须针对Proxy实例进行操作，而不是针对目标对象（上例是空对象）进行操作。
+ */
+// 如果handler没有设置任何拦截，那就等同于直接通向原对象。
+{
+  let target = {};
+  let handler = {};
+  let proxy = new Proxy(target, handler);
+
+  proxy.a = 'b';
+  console.log('target.a = ', target.a);
+
+  divider();
+}
+
+/**
+ * Proxy 实例也可以作为其他对象的原型对象。
+ */
+{
+  var proxy = new Proxy({}, {
+    get: function (target, property) {
+      return 35;
+    }
+  });
+
+  let obj = Object.create(proxy);
+
+  console.log('obj:', obj);
+  console.log(obj.time); // 35
+
+  divider();
+}
