1b65839cc10b7a786876720d5e89aadf83576f7b
qinWenMeng <qwm668@gmail.com>
2019-08-23 13:03:32 +0800

    --promise

--- ES6/Promise/Promise.js
+++ ES6/Promise/Promise.js
@@ -1,3 +1,16 @@
-new Promise((resolve, reject) => {
-
-});
+const getUsers = async () => {
+  const users = await fetch('http://jsonplaceholder.typicode.com/users')
+    .then(res => res.json());
+  return fetch(`http://jsonplaceholder.typicode.com/users/${users[0].id}`);
+};
+const getUser = () => {
+  return new Promise((resolve, reject) => {
+    getUsers()
+      .then(res => res.json())
+      .then(res => resolve(res))
+      .catch(err => reject(err));
+  });
+};
+getUser()
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
