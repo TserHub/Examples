d0e91ba97a478759a7f42afa4baff11abaf15b56
qinWenMeng <qwm668@qq.com>
2019-09-18 18:15:15 +0800

    --promise all

--- ES6/Promise/Promise.js
+++ ES6/Promise/Promise.js
@@ -1,3 +1,4 @@
+// 继发执行
 const getUsers = async () => {
   const users = await fetch('http://jsonplaceholder.typicode.com/users')
     .then(res => res.json());
@@ -14,3 +15,13 @@ const getUser = () => {
 getUser()
   .then(res => console.log(res))
   .catch(err => console.error(err));
+
+// 并发执行
+const ids = [1, 2, 3];
+const promiseAll = () => {
+  const requests = ids.map((id) => {
+    return fetch(`http://jsonplaceholder.typicode.com/users/${id}`).then(res => res.json());
+  });
+  return Promise.all(requests);
+};
+promiseAll().then(res => console.log(res));
